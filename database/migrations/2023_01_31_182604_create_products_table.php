0308e3dea03cd32bff182bc02d03859b29075e34
chowdhury-jamil-jc-787 <17-34747-2@student.aiub.edu>
2023-02-01 09:08:31 +0600

    first commit

---
+++ database/migrations/2023_01_31_182604_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('code')->primary();
+            $table->text('name');
+            $table->string('categories');
+            $table->string('labels');
+            $table->string('image');
+            $table->integer('rating')->nullable();
+            $table->string('availability');
+            $table->string('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
